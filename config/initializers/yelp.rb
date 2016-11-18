fb08edf89d0e1392409b12cf03cb93d2d7084ce6
Daniel Salinetti <dansalinetti@gmail.com>
2016-11-18 10:21:50 -0500

    Adding yelp initializer with access tokens

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,7 @@
+require 'yelp'
+
+client = Yelp::Client.new({ consumer_key: oqIC9sXm3wqe-U_Tsv_AZQ,
+                            consumer_secret: Ep7IIgzYiHry8JhMP52IzNzLoK4,
+                            token: rwqasoqe13MJcThcNoKGzxCXi4IA5Mm4,
+                            token_secret: mTyUxZFl2atH6GbkdsdTybIMfWs
+                          })
